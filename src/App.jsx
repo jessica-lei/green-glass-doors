835af35e5b0de17f7cc27a0d7fe0005a5cace0df
Jessica Lei <jeslei@publicisgroupe.net>
2023-09-25 12:58:06 -0500

    initial commit, app skeleton

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [modalShow, setModalShow] = useState(false);
+
+  return (
+    <div className="App">
+      <div className="hero">
+        <h3>welcome to the</h3>
+        <h1>GREEN GLASS DOORS</h1>
+        <h3>what are you bringing in?</h3>
+      </div>
+      <input className="input" />
+    </div>
+    // <>
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </>
+  )
+}
+
+export default App
